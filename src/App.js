794d6889b9d4eaf3b46fb09c604e1f17932ac488
Maxime <maximeth@MacBookDeMaxime.local>
2023-10-17 10:17:23 +0200

    Added auth features, as well as a logout feature and button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SignUp from "/components/SignUp";
+import SignIn from "/components/SignIn";
+import { auth } from "./firebase";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((currentUser) => {
+      if (currentUser) {
+        setUser(currentUser);
+      } else {
+        setUser(null);
+      }
+    });
+
+    // On se désabonne du l'écouteur lors du nettoyage
+    return () => unsubscribe();
+  }, []);
+
+  const handleSignOut = async () => {
+    try {
+      await auth.signOut();
+    } catch (error) {
+      console.error("Erreur lors de la déconnexion :", error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        {user ? (
+          <>
+            <p>Bonjour, {user.email}</p>
+            {/* Ajouter ici que les composants et routes qui seront accessibles uniquement aux users connectés */}
+          </>
+        ) : (
+          <Switch>
+            <Route path="/signup" component={SignUp} />
+            <Route path="/signin" component={SignIn} />
+          </Switch>
+        )}
+      </div>
+    </Router>
   );
 }
 
